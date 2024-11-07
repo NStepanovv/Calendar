05defa51ce9604d1dd47ec137a6677bd65f1bfb2
NStepanovv <noobfrog581@gmail.com>
2024-11-07 10:45:20 +0300

    deleted media, changed logo

--- calendar_qr/calendar_qr/qr_script.py
+++ calendar_qr/calendar_qr/qr_script.py
@@ -3,11 +3,11 @@ from django.conf import settings
 
 def generate_qr_codes():
     #base_url = 'http://localhost:3000/week/'  # Базовый URL для видео
-    base_url = 'http://192.168.1.42:3000/week/'  # Базовый URL для видео
+    base_url = 'http://192.168.1.49:3000/week/'  # Базовый URL для видео
     output_dir = 'calendar_qr/media/qrcodes/'  # Папка для сохранения QR-кодов
 
     # Генерируем QR-коды для 52 недель
-    for week in range(3, 4):
+    for week in range(1, 5):
         url = f"{base_url}{week}"  # Ссылка для каждой недели
         img = qrcode.make(url)
         img.save(f'{output_dir}week_{week}.png')  # Сохраняем QR-код как PNG
